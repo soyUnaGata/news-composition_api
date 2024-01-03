696c59c347d4c567b25b66456d2b8e4e95b89c97
soyUnaGata <fallen.snitch@gmail.com>
2024-01-03 12:31:39 +0200

    implement axios and get response

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,63 @@
 <template>
+  <div class="wrapper">
+    <header class="d-flex flex-column align-items-center">
+      <h1>By New York Times</h1>
+      <div class="subtitle d-flex">
+        <p class="subtitle__section">Business Section |</p>
+        <p class="subtitle__date">Last Update:</p>
+      </div>
+    </header>
 
+    <main>
+      <div class="news__wrapper">
+        <news-list />
+      </div>
+    </main>
+  </div>
 </template>
 
 <script setup>
+import { ref, onMounted } from "vue";
+import axios from "axios";
+
+import NewsList from "./components/NewsList.vue";
+
+const news = ref([]);
+
+onMounted(async () => {
+  try {
+    const data = await axios.get('https://api.nytimes.com/svc/topstories/v2/business.json?api-key=ZhZXYAVIC3MhEys9Us9Y5icDqq46GFRF');
+    console.log(data)
+
+  } catch (err) {
+    console.log(err)
+  }
+})
+
 
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style lang="scss">
+// #app {}
+
+header {
+  padding: 20px 0;
+
+  h1 {
+    font-weight: 800;
+  }
+}
+
+.subtitle {
+  gap: 5px;
+  padding-top: 10px;
+
+  &__section {
+    font-weight: 500;
+  }
+
+  &__date {
+    font-weight: 300;
+  }
 }
 </style>
