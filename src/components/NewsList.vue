a0bdd96d99598c7315952b7a41b4a6ab1570bcab
soyUnaGata <fallen.snitch@gmail.com>
2024-01-03 12:29:23 +0200

    implement newsList components

---
+++ src/components/NewsList.vue
@@ -0,0 +1,22 @@
+<template>
+  <ul class="news__list">
+    <li class="news__list-item">
+      <a class="news__list-img">
+        <!-- <img class="news__list-item-img" src="./assets/" alt=""> -->
+      </a>
+      <div class="news__list-about">
+        <div class="news__list-details">
+          <h3 class="news__list-title"></h3>
+          <p class="subtitle__date">Last Update:</p>
+        </div>
+        <h4 class="news__list-author"></h4>
+        <p class="news__list-briefly"></p>
+      </div>
+      <h4 class="news__list-categoryies">
+        <ul class="news__list-categoryies-item">
+          <li></li>
+        </ul>
+      </h4>
+    </li>
+  </ul>
+</template>
