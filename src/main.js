f0791c81200de89229b1647bd6f98f56e7494e7d
soyUnaGata <fallen.snitch@gmail.com>
2024-01-03 12:28:31 +0200

    implement fonts and style reset

--- src/main.js
+++ src/main.js
@@ -1,4 +1,9 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from 'vue';
+import App from './App.vue';
+import "@/assets/scss/__fonts.scss";
+import "@/assets/scss/__global.scss";
+import "@/assets/scss/__reset.scss";
+import "@/assets/scss/__vars.scss";
+import "@/assets/scss/main.scss";
 
 createApp(App).mount('#app')
